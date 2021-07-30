9c7ef3cb2c6dfd202015f5ddd33dfd5108619efe
Ramiro Linde Rodríguez <55402909+RamiroLinde@users.noreply.github.com>
2021-07-30 11:50:14 +0200

    creadon pacientes

---
+++ routes/pacientes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { create } = require('../models/paciente.model');
+const router = express.Router();
+
+router.get('/new', (req, res)=>{
+    res.render('pacientes/new');
+});
+
+router.post('/create',async (req, res)=>{
+    try{
+        await create(req.body);
+        res.redirect('/pacientes');
+    }catch(error){
+        console.log(error);
+    }
+    
+});
+
+module.exports = router;
