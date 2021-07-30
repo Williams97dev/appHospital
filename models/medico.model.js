e1d9a5a1eaa68ddb0b9ed7ef158accad7393b8d1
Juan Gutierrez <gutierrezbj@gmail.com>
2021-07-30 09:21:24 +0200

    no message

--- models/medico.model.js
+++ models/medico.model.js
@@ -6,6 +6,27 @@ const getAll = () => {
     );
 };
 
+const create = ({ name, email, address, phone }) => {
+    return executeQuery(
+        'insert into clients (name, apellido, especialidad, telefono) values (?, ?, ?, ?)',
+        [nombre, apellido, especialidad, telefono]
+    );
+};
+
+// const getBypacienteId = (userId) => {
+//     return executeQuery (
+//         'select * from clients where user_id = ?',
+//         [userId]
+//     );
+// }
+
+// const getBymedicoId = (userId) => {
+//     return executeQuery (
+//         'select * from clients where user_id = ?',
+//         [userId]
+//     );
+// }
+
 module.exports = {
-    getAll
+    getAll, create
 };
--- routes/api/medicos.js
+++ routes/api/medicos.js
@@ -12,17 +12,25 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.post('/', (req, res) => {
-    res.send(' post api medicos');
+router.put('/', async (req, res) => {
+    try {
+        const result = await create(req.body);
+        res.json(result);
+    } catch (error) {
+        res.json({ error: error.message });
+    }
 });
 
-router.put('/:idMedico', (req, res) => {
-    res.send('put api medicos');
-});
 
-router.delete('/:idMedico', (req, res) => {
-    res.send('delete api medicos');
-});
+
+
+// router.put('/:idMedico', (req, res) => {
+//     res.send('put api medicos');
+// });
+
+// router.delete('/:idMedico', (req, res) => {
+//     res.send('delete api medicos');
+// });
 
 
 module.exports = router;
